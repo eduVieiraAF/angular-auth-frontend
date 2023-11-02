7823ba9275e52120f41a17ecb7ea709ce2b3274d
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-02 14:40:35 -0300

    refactoring service + logout

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, map, catchError, throwError } from 'rxjs';
 
 @Injectable({
@@ -8,14 +9,29 @@ import { Observable, map, catchError, throwError } from 'rxjs';
 export class AuthService {
   private url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
   public login(payload: { email: string, password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
-      map((data) => console.log(data)),
-      catchError((err) => {
-        return throwError(() => err.error.message);
-      })
-    );
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
+      map((res) => {
+        localStorage.removeItem('auth_token');
+        localStorage.setItem('auth_token', res.token)
+
+        return this.router.navigate(['admin']);
+      }),
+        catchError((err) => {
+          if (err.error.message) return throwError(() => err.error.message);
+
+          return throwError(() => "Something went wrong  =[");
+        })
+      )
+  }
+
+  public logout() {
+    localStorage.removeItem('auth_token');
+    this.router.navigate(['']);
   }
 }
