1d56c25af203102e5170b87c0b0898e0076c3bd8
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-02 08:32:03 -0300

    erro page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './core/components/error-page/error-page.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
   }
 ];
 
